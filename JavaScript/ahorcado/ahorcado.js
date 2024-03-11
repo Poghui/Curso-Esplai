ff437330015a06bedc7d2ac2f31f829677c762d8
Poghui <poghui4@gmail.com>
2024-03-11 18:42:25 +0100

    ahorcado

---
+++ JavaScript/ahorcado/ahorcado.js
@@ -0,0 +1,34 @@
+//6:Juego de ahorcado
+const replaceAt = (string, character, index) => {
+    return string.substring(0, index) + character + string.substring(index + character.length);
+}
+
+let hemosGanado = false;
+
+const words = ['hola', 'mama', 'papa', 'tarde'];
+const palabraSecreta = words[Math.floor(Math.random() * words.length)];
+let palabraConGuiones = palabraSecreta.replace(/./g, "_ ");
+
+
+
+while (!hemosGanado) {
+    alert(palabraConGuiones)
+
+    const letraIntroducida = prompt('Introduce una letra');
+
+    let letraEncontrada = false;
+
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        if (palabraSecreta[i] === letraIntroducida) {
+            palabraConGuiones = replaceAt(palabraConGuiones, letraIntroducida, i * 2);
+            letraEncontrada = true;
+        }
+    }
+    if (!letraEncontrada) {
+        alert('Has fallado');
+    }
+    if (palabraConGuiones.indexOf("_") == -1) {
+        alert("¡Has ganado!, esta era la palabra ' " + palabraConGuiones.replace(/\s/g, '') + " '")
+        hemosGanado = true;
+    }
+}
